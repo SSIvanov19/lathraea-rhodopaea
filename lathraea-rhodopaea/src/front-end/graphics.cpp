6f3b58b45fa5074aae22c2058f1255c218a3a9f4
Mariya Ilcheva <MZIlcheva19@codingburgas.bg>
2022-01-29 00:59:16 +0200

    Add graphics  - Opened book  - Sword with snake

---
+++ lathraea-rhodopaea/src/front-end/graphics.cpp
@@ -0,0 +1,92 @@
+/*! @file graphics.cpp
+*   @brief A source file for the graphics.
+*/
+
+#include <iostream>
+
+/**
+ * @brief Function for printing the menu base
+ * @param None
+ * @return Void
+*/
+void printBook()
+{
+	std::cout << "                         . " << char(196) << " " << char(196) << " " << char(196) << " " << char(196) << char(196) << char(196) << char(196) << " " << char(196) << " " << char(196) << " " << char(196) << char(196) << char(196) << " .                       . " << char(196) << char(196) << char(196) << " " << char(196) << " " << char(196) << " " << char(196) << char(196) << char(196) << char(196) << " " << char(196) << " " << char(196) << " " << char(196) << " ." << std::endl;
+	std::cout << "                   " << char(196) << char(196) << char(196) << " '                         " << "' ' " << char(196) << " .     . " << char(196) << " ' '                         ' " << char(196) << char(196) << char(196) << std::endl;
+	std::cout << "  " << char(201) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << " " << char(196) << " " << char(196) << " " << char(196) << " '                                      \\   /                                      ' " << char(196) << " " << char(196) << " " << char(196) << " " << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(205) << char(187) << std::endl;
+	std::cout << char(218) << char(196) << char(186) << "                                                      \\ /                                                      " << char(186) << char(196) << char(191) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                                                       |                                                       " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "                   " << char(250) << " ' " << char(196) << " " << char(196) << " " << char(196) << " " << char(196) << " " << char(196) << " " << char(196) << " " << char(196) << " " << char(196) << " " << char(196) << " ' " << char(250) << " .         |         . " << char(250) << " ' " << char(196) << " " << char(196) << " " << char(196) << " " << char(196) << " " << char(196) << " " << char(196) << " " << char(196) << " " << char(196) << " " << char(196) << " ' " << char(250) << "                   " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(186) << "              " << char(196) << char(196) << char(196) << char(196) << char(196) << "   " << " ' " << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << " ' " << char(250) << " .    " << char(196) << char(196) << " " << char(250) << " .  |  . " << char(250) << " " << char(196) << char(196) << "    . " << char(250) << " ' " << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << " '    " << char(196) << char(196) << char(196) << char(196) << char(196) << "              " << char(186) << " " << char(179) << std::endl;
+	std::cout << char(179) << " " << char(200) << char(205) << char(205) << char(205) << char(205) << char(205) << " " << char(205) << " " << char(205) << " " << char(205) << " ' " << char(196) << " " << char(196) << " " << char(196) << " " << char(196) << " " << char(250) << "                     " << char(196) << " " << char(196) << " " << char(250) << " .  \\ | /  . " << char(250) << " " << char(196) << " " << char(196) << "                     " << char(250) << " " << char(196) << " " << char(196) << " " << char(196) << " " << char(196) << " ' " << char(205) << " " << char(205) << " " << char(205) << " " << char(205) << char(205) << char(205) << char(205) << char(205) << char(188) << " " << char(179) << std::endl;
+	std::cout << char(192) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << " \\         / " << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(217) << std::endl;
+	std::cout << "                                                      ' " << char(196) << char(196) << char(196) << char(196) << char(196) << " '" << std::endl;
+}
+
+/**
+ * @brief Function for printing a sword with a snake
+ * @param None
+ * @return Void
+*/
+void printSnakeSword()
+{
+	std::cout << "                               ,-." << std::endl;
+	std::cout << "                              (\"O_)" << std::endl;
+	std::cout << "                             / `-/" << std::endl;
+	std::cout << "                            /-. /" << std::endl;
+	std::cout << "                           /   )" << std::endl;
+	std::cout << "                          /   /  " << std::endl;
+	std::cout << "              _          /-. /" << std::endl;
+	std::cout << "             (_)\"-.._   /   )" << std::endl;
+	std::cout << "              \"-._   '\"\"( )/    " << std::endl;
+	std::cout << "                   \"-/\"-._\"`. " << std::endl;
+	std::cout << "                    /     \"-.'._" << std::endl;
+	std::cout << "                   /\\       /-._\"-._" << std::endl;
+	std::cout << "    _,---...__    /  ) _,-\"/    \"-(_)" << std::endl;
+	std::cout << "___<__(|) _   \"\"-/  / /   /" << std::endl;
+	std::cout << " '  `----' \"\"-.   \\/ /   /" << std::endl;
+	std::cout << "               )  ] /   /" << std::endl;
+	std::cout << "       ____..-'   //   /                       )" << std::endl;
+	std::cout << "   ,-\"\"      __.,'/   /   ___                 /" << std::endl;
+	std::cout << "  /    ,--\"\"/  / /   /,-\"\"   \"\"\"-          ,'/" << std::endl;
+	std::cout << " [    (    /  / /   /  ,.---,_   `._   _,-','" << std::endl;
+	std::cout << "  \\    `-./  / /   /  /       `-._  \"\"\"_.-'" << std::endl;
+	std::cout << "   `-._  /  / /   /_,'            \"\"--\"" << std::endl;
+	std::cout << "       \"/  / /   /\"         " << std::endl;
+	std::cout << "       /  / /   /" << std::endl;
+	std::cout << "      /  / /   /  " << std::endl;
+	std::cout << "     /  |,'   /  " << std::endl;
+	std::cout << "    :   /    /" << std::endl;
+	std::cout << "    [  /   ,'  " << std::endl;
+	std::cout << "    | /  ,'" << std::endl;
+	std::cout << "    |/,-'" << std::endl;
+	std::cout << "    /" << std::endl;
+}
