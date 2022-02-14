afb1f487f4c8235d626fe777b1f4204455ce4d43
VBDekova19 <VBDekova19@codingburgas.bg>
2022-02-14 23:09:01 +0200

    Enhance function printMainPage()

--- lathraea-rhodopaea/include/front-end/graphics.h
+++ lathraea-rhodopaea/include/front-end/graphics.h
@@ -4,31 +4,32 @@
 
 #pragma once
 
+void printMainPage();// Function for printing main page for the program.
 void printClosedBook();// Function for printing closed book.
 void printSlightlyOpenBook();// Function for printing slightly open book.
 void printHalfOpenBook();//Function for half open book.
 void printAlmostOpenedBook();// Function for printing almost open book.
 void printFullyOpenedBook();// Function for printing fully open book.
-void printOpenedBook(); // Function for printing the opened book
-void printOnePageLeafedBook(); // Function for printing the book with only one page leafed
-void pageLeafingLeftOne(); // Function for printing the first part of the book's leafing to the left
-void pageLeafingRightOne(); // Function for printing the first part of the book's leafing to the right
-void pageLeafingLeftTwo(); // Function for printing the second part of the book's leafing to the left
-void pageLeafingRightTwo(); // Function for printing the second part of the book's leafing to the right
-void printSnakeSword(); // Function for printing a sword with a snake
-void printBulgarianMap(); // Function for printing the map of Bulgaria
-void printPopUp(); // Function for printing a pop-up tab
-void printMapPopUp(); // Function for printing a pop-up tab for the map
-void printTimeline(); // Function for printing a timeline
-void printTimelinePopUp(); // Function for printing a pop-up tab for the timeline
-void printSwordTimelinePopUp(); // Function for printing a pop-up tab for the sword timeline
-void printSwordTimeline(); // Function for printing a sword timeline
-void printTimeline(); // Function for printing a timeline
-void printaboutUs();// Function for printing about us page
-void printText();// Function for printing printing text for about us page 
-void logoForProgram(); // Function for printing the logo of the project
-void printTeamLogo(); // Function for printing the team logo
-void printTeamKeyboardLogo(); // Function for printing the team logo as keyboard
-void printBlankSpace(int x, int y); // Function for printing blank space
-void prinyBookDecorations(); // Function for printing book decorations
+void printOpenedBook(); // Function for printing the opened book.
+void printOnePageLeafedBook(); // Function for printing the book with only one page leafed.
+void pageLeafingLeftOne(); // Function for printing the first part of the book's leafing to the left.
+void pageLeafingRightOne(); // Function for printing the first part of the book's leafing to the right.
+void pageLeafingLeftTwo(); // Function for printing the second part of the book's leafing to the left.
+void pageLeafingRightTwo(); // Function for printing the second part of the book's leafing to the right.
+void printSnakeSword(); // Function for printing a sword with a snake.
+void printBulgarianMap(); // Function for printing the map of Bulgaria.
+void printPopUp(); // Function for printing a pop-up tab.
+void printMapPopUp(); // Function for printing a pop-up tab for the map.
+void printTimeline(); // Function for printing a timeline.
+void printTimelinePopUp(); // Function for printing a pop-up tab for the timeline.
+void printSwordTimelinePopUp(); // Function for printing a pop-up tab for the sword timeline.
+void printSwordTimeline(); // Function for printing a sword timeline.
+void printTimeline(); // Function for printing a timeline.
+void printaboutUs();// Function for printing about us page.
+void printText();// Function for printing printing text for about us page.
+void logoForProgram(); // Function for printing the logo of the project.
+void printTeamLogo(); // Function for printing the team logo.
+void printTeamKeyboardLogo(); // Function for printing the team logo as keyboard.
+void printBlankSpace(int x, int y); // Function for printing blank space.
+void prinyBookDecorations(); // Function for printing book decorations.
 
